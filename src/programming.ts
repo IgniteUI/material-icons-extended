30a98e8bf8ba821e7d08e2cd4fd37a95952e7249
Simeon Simeonoff <sim.simeonoff@gmail.com>
2020-07-27 17:23:22 +0300

    feat(*): add programming and social media categories

---
+++ src/programming.ts
@@ -0,0 +1,214 @@
+import { IMXIcon, IconCategory } from "./common/icon";
+
+export const api: IMXIcon = {
+  name: "api",
+  value: require("../svgs/programming/api.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const appSymbol: IMXIcon = {
+  name: "app-symbol",
+  value: require("../svgs/programming/app-symbol.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const bug: IMXIcon = {
+  name: "bug",
+  value: require("../svgs/programming/bug.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const codeFork: IMXIcon = {
+  name: "code-fork",
+  value: require("../svgs/programming/code-fork.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const codeReview: IMXIcon = {
+  name: "code-review",
+  value: require("../svgs/programming/code-review.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const code: IMXIcon = {
+  name: "code",
+  value: require("../svgs/programming/code.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const terminal: IMXIcon = {
+  name: "terminal",
+  value: require("../svgs/programming/console.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const development: IMXIcon = {
+  name: "development",
+  value: require("../svgs/programming/development.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const fileChanges: IMXIcon = {
+  name: "file-changes",
+  value: require("../svgs/programming/file-changes.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const fileTree: IMXIcon = {
+  name: "file-tree",
+  value: require("../svgs/programming/file-tree.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const gitBranch: IMXIcon = {
+  name: "git-branch",
+  value: require("../svgs/programming/git-branch.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const gitCommit: IMXIcon = {
+  name: "git-commit",
+  value: require("../svgs/programming/git-commit.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const gitCompare: IMXIcon = {
+  name: "git-compare",
+  value: require("../svgs/programming/git-compare.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const gitIssue: IMXIcon = {
+  name: "git-issue",
+  value: require("../svgs/programming/git-issue.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const gitMerge: IMXIcon = {
+  name: "git-merge",
+  value: require("../svgs/programming/git-merge.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const grid: IMXIcon = {
+  name: "grid",
+  value: require("../svgs/programming/grid.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const inspectCode: IMXIcon = {
+  name: "inspect-code",
+  value: require("../svgs/programming/inspect-code.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const milestone: IMXIcon = {
+  name: "milestone",
+  value: require("../svgs/programming/milestone.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const codeModule: IMXIcon = {
+  name: "code-module",
+  value: require("../svgs/programming/module.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const plugin: IMXIcon = {
+  name: "plugin",
+  value: require("../svgs/programming/plugin.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const pullRequest: IMXIcon = {
+  name: "pull-request",
+  value: require("../svgs/programming/pull-request.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const repository: IMXIcon = {
+  name: "repository",
+  value: require("../svgs/programming/repository.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const responsiveLayout: IMXIcon = {
+  name: "responsive-layout",
+  value: require("../svgs/programming/responsive-layout.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const severity: IMXIcon = {
+  name: "severity",
+  value: require("../svgs/programming/severity.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const softwareArchitecture: IMXIcon = {
+  name: "softuare-architecture",
+  value: require("../svgs/programming/software-architecture.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const softwareInstaller: IMXIcon = {
+  name: "software-installer",
+  value: require("../svgs/programming/software-installer.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const sourceCode: IMXIcon = {
+  name: "source-code",
+  value: require("../svgs/programming/source-code.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const systemUpdate: IMXIcon = {
+  name: "system-update",
+  value: require("../svgs/programming/system-update.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const verifiedAccount: IMXIcon = {
+  name: "verified-account",
+  value: require("../svgs/programming/verified-account.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export const workflow: IMXIcon = {
+  name: "workflow",
+  value: require("../svgs/programming/workflow.svg") as string,
+  categories: [IconCategory.Programming],
+};
+
+export default [
+  api,
+  appSymbol,
+  bug,
+  code,
+  codeFork,
+  codeModule,
+  codeReview,
+  development,
+  fileChanges,
+  fileTree,
+  gitBranch,
+  gitCommit,
+  gitCommit,
+  gitIssue,
+  gitMerge,
+  grid,
+  inspectCode,
+  milestone,
+  plugin,
+  pullRequest,
+  repository,
+  responsiveLayout,
+  severity,
+  softwareArchitecture,
+  softwareInstaller,
+  sourceCode,
+  systemUpdate,
+  terminal,
+  verifiedAccount,
+  workflow
+];
---
+++ src/socialMedia.ts
@@ -0,0 +1,130 @@
+import { IMXIcon, IconCategory } from "./common/icon";
+
+export const behance: IMXIcon = {
+  name: "behance",
+  value: require("../svgs/social-media/behance.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const dribble: IMXIcon = {
+  name: "dribble",
+  value: require("../svgs/social-media/dribble.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const facebookMessenger: IMXIcon = {
+  name: "facebook-messenger",
+  value: require("../svgs/social-media/facebook-messenger.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const facebook: IMXIcon = {
+  name: "facebook",
+  value: require("../svgs/social-media/facebook.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const github: IMXIcon = {
+  name: "github",
+  value: require("../svgs/social-media/github.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const instagram: IMXIcon = {
+  name: "instagram",
+  value: require("../svgs/social-media/instagram.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const linkedin: IMXIcon = {
+  name: "linkedin",
+  value: require("../svgs/social-media/linkedin.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const medium: IMXIcon = {
+  name: "medium",
+  value: require("../svgs/social-media/medium.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const pinterest: IMXIcon = {
+  name: "pinterest",
+  value: require("../svgs/social-media/pinterest.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const reddit: IMXIcon = {
+  name: "reddit",
+  value: require("../svgs/social-media/reddit.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const snapchat: IMXIcon = {
+  name: "snapchat",
+  value: require("../svgs/social-media/snapchat.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const telegram: IMXIcon = {
+  name: "telegram",
+  value: require("../svgs/social-media/telegram.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const tiktok: IMXIcon = {
+  name: "tiktok",
+  value: require("../svgs/social-media/tiktok.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const twitter: IMXIcon = {
+  name: "twitter",
+  value: require("../svgs/social-media/twitter.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const viber: IMXIcon = {
+  name: "viber",
+  value: require("../svgs/social-media/viber.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const wechat: IMXIcon = {
+  name: "wechat",
+  value: require("../svgs/social-media/wechat.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const whatsapp: IMXIcon = {
+  name: "whatsapp",
+  value: require("../svgs/social-media/whatsapp.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export const youtube: IMXIcon = {
+  name: "youtube",
+  value: require("../svgs/social-media/youtube.svg") as string,
+  categories: [IconCategory.SocialMedia],
+};
+
+export default [
+  behance,
+  dribble,
+  facebookMessenger,
+  facebook,
+  github,
+  instagram,
+  linkedin,
+  medium,
+  pinterest,
+  reddit,
+  snapchat,
+  telegram,
+  tiktok,
+  twitter,
+  viber,
+  wechat,
+  whatsapp,
+  youtube
+];
