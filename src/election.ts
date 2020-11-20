6388a8416ab2dd2f1fc0513caba076973f213fe2
S Dimchevski <sdimchevski@infragistics.com>
2020-11-20 13:56:13 +0200

    Addition of new election icons

--- src/election.ts
+++ src/election.ts
@@ -6,9 +6,57 @@ export const aggregation: IMXIcon = {
   categories: ["election"],
 };
 
+export const candidateResults: IMXIcon = {
+    name: "candidate-results",
+    value: require("./svgs/candidate-results.svg"),
+    categories: ["election"],
+};
+
+export const debate: IMXIcon = {
+    name: "debate",
+    value: require("./svgs/debate.svg"),
+    categories: ["election"],
+};
+
 export const electoralVotes: IMXIcon = {
     name: "electoral-votes",
     value: require("./svgs/electoral-votes.svg"),
+    categories: ["election"],
+};
+
+export const euParliament: IMXIcon = {
+    name: "eu-parliament",
+    value: require("./svgs/eu-parliament.svg"),
+    categories: ["election"],
+};
+
+export const femaleCandidate: IMXIcon = {
+    name: "female-candidate",
+    value: require("./svgs/female-candidate.svg"),
+    categories: ["election"],
+};
+
+export const horn: IMXIcon = {
+    name: "horn",
+    value: require("./svgs/horn.svg"),
+    categories: ["election"],
+};
+
+export const maleCandidate: IMXIcon = {
+    name: "male-candidate",
+    value: require("./svgs/male-candidate.svg"),
+    categories: ["election"],
+};
+
+export const parliament1: IMXIcon = {
+    name: "parliament-1",
+    value: require("./svgs/parliament-1.svg"),
+    categories: ["election"],
+};
+
+export const parliament2: IMXIcon = {
+    name: "parliament-2",
+    value: require("./svgs/parliament-2.svg"),
     categories: ["election"],
 };
 
@@ -16,11 +64,41 @@ export const popularVotes: IMXIcon = {
     name: "popular-votes",
     value: require("./svgs/popular-votes.svg"),
     categories: ["election"],
-};
+}
+
+export const protesterSigns: IMXIcon = {
+    name: "protester-signs",
+    value: require("./svgs/protester-signs.svg"),
+    categories: ["election"],
+}
 
 export const resultsByCandidates: IMXIcon = {
     name: "results-by-candidates",
     value: require("./svgs/results-by-candidates.svg"),
+    categories: ["election"],
+};
+
+export const resultsPiechart: IMXIcon = {
+    name: "results-piechart",
+    value: require("./svgs/results-piechart.svg"),
+    categories: ["election"],
+};
+
+export const scalesBalanced: IMXIcon = {
+    name: "scales-balanced",
+    value: require("./svgs/scales-balanced.svg"),
+    categories: ["election"],
+};
+
+export const scalesUnbalanced: IMXIcon = {
+    name: "scales-unbalanced",
+    value: require("./svgs/scales-unbalanced.svg"),
+    categories: ["election"],
+};
+
+export const stats: IMXIcon = {
+    name: "stats",
+    value: require("./svgs/stats.svg"),
     categories: ["election"],
 };
 
@@ -84,11 +162,42 @@ export const usaTx: IMXIcon = {
     categories: ["election"],
 };
 
+export const vote: IMXIcon = {
+    name: "vote",
+    value: require("./svgs/vote.svg"),
+    categories: ["election"],
+};
+
+export const whiteHouse1: IMXIcon = {
+    name: "white-house-1",
+    value: require("./svgs/white-house-1.svg"),
+    categories: ["election"],
+};
+
+export const whiteHouse2: IMXIcon = {
+    name: "white-house-2",
+    value: require("./svgs/white-house-2.svg"),
+    categories: ["election"],
+};
+
 export const elections = [
     aggregation,
+    candidateResults,
+    debate,
     electoralVotes,
+    euParliament,
+    femaleCandidate,
+    horn,
+    maleCandidate,
+    parliament1,
+    parliament2,
     popularVotes,
+    protesterSigns,
     resultsByCandidates,
+    resultsPiechart,
+    scalesBalanced,
+    scalesUnbalanced,
+    stats,
     timeline,
     usaCa,
     usaFl,
@@ -99,4 +208,7 @@ export const elections = [
     usaPartyLibertarian,
     usaPartyRepublican,
     usaTx,
+    vote,
+    whiteHouse1,
+    whiteHouse2,
 ];
