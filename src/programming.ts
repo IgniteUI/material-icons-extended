fe722bd148a947d32d20731d103c9c56f6ef0b9b
Stefan Ivanov <sivanov@infragistics.com>
2021-04-23 16:35:48 +0300

    Adding keywords for programming

--- src/programming.ts
+++ src/programming.ts
@@ -4,210 +4,210 @@ export const api: IMXIcon = {
   name: "api",
   value: require("./svgs/api.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["api", "development", "interface", "service", "database", "programming"],
 };
 
 export const appSymbol: IMXIcon = {
   name: "app-symbol",
   value: require("./svgs/app-symbol.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["appsymbol", "app", "development", "programming", "circle", "a"],
 };
 
 export const bug: IMXIcon = {
   name: "bug",
   value: require("./svgs/bug.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["bug", "animal", "insect", "virus", "malware", "security", "development", "programming"],
 };
 
 export const codeFork: IMXIcon = {
   name: "code-fork",
   value: require("./svgs/code-fork.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["codefork", "code", "fork", "git", "github", "development", "collaboration", "control", "version", "programming"],
 };
 
 export const codeReview: IMXIcon = {
   name: "code-review",
   value: require("./svgs/code-review.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["codereview", "review", "software", "app", "development", "programming"],
 };
 
 export const code: IMXIcon = {
   name: "code",
   value: require("./svgs/code.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["code", "script", "source", "development", "programming"],
 };
 
 export const terminal: IMXIcon = {
   name: "terminal",
   value: require("./svgs/console.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["terminal", "console", "code", "bash", "cmd", "command", "sh", "shell", "development", "programming"],
 };
 
 export const development: IMXIcon = {
   name: "development",
   value: require("./svgs/development.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["puzzle", "pieces", "jigsaw", "combination", "play", "game", "toy", "development", "programming"],
 };
 
 export const fileChanges: IMXIcon = {
   name: "file-changes",
   value: require("./svgs/file-changes.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["file", "changes", "plus", "minus", "development", "programming"],
 };
 
 export const fileTree: IMXIcon = {
   name: "file-tree",
   value: require("./svgs/file-tree.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["file", "tree", "hierarchy", "files", "system", "levels", "development", "programming", "chart", "organize", "structure", "treeview"],
 };
 
 export const gitBranch: IMXIcon = {
   name: "git-branch",
   value: require("./svgs/git-branch.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["git", "branch", "development", "programming", "commit", "fork", "split", "code", "collaboration"],
 };
 
 export const gitCommit: IMXIcon = {
   name: "git-commit",
   value: require("./svgs/git-commit.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["git", "commit", "development", "programming", "pull", "code", "collaboration"],
 };
 
 export const gitCompare: IMXIcon = {
   name: "git-compare",
   value: require("./svgs/git-compare.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["git", "compare", "development", "programming", "code", "collaboration"],
 };
 
 export const gitIssue: IMXIcon = {
   name: "git-issue",
   value: require("./svgs/git-issue.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["git", "issue", "development", "programming", "warning", "exclamation"],
 };
 
 export const gitMerge: IMXIcon = {
   name: "git-merge",
   value: require("./svgs/git-merge.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["git", "merge", "development", "programming", "code", "collaboration"],
 };
 
 export const grid: IMXIcon = {
   name: "grid",
   value: require("./svgs/grid.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["grid", "table", "development", "programming", "component", "datatable"],
 };
 
 export const inspectCode: IMXIcon = {
   name: "inspect-code",
   value: require("./svgs/inspect-code.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["inspect", "code", "development", "programming", "marketing", "markup", "search", "seo", "review"],
 };
 
 export const milestone: IMXIcon = {
   name: "milestone",
   value: require("./svgs/milestone.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["milestone", "sign", "development", "programming", "travel", "roadmap", "tourism"],
 };
 
 export const codeModule: IMXIcon = {
   name: "code-value",
   value: require("./svgs/module.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["code", "value", "development", "programming", "module", "chip", "cpu", "electronics", "equipment", "hardware", "iot"],
 };
 
 export const plugin: IMXIcon = {
   name: "plugin",
   value: require("./svgs/plugin.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["plugin", "plug", "development", "programming", "socket", "connector", "electricity"],
 };
 
 export const pullRequest: IMXIcon = {
   name: "pull-request",
   value: require("./svgs/pull-request.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["pull", "request", "development", "programming", "git", "commit", "scheme"],
 };
 
 export const repository: IMXIcon = {
   name: "repository",
   value: require("./svgs/repository.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["repo", "repository", "development", "programming", "cabinet", "document", "folder", "storage"],
 };
 
 export const responsiveLayout: IMXIcon = {
   name: "responsive-layout",
   value: require("./svgs/responsive-layout.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["responsive", "layout", "development", "programming", "device", "adaptive", "screen", "fluid"],
 };
 
 export const severity: IMXIcon = {
   name: "severity",
   value: require("./svgs/severity.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["severity", "gauge", "development", "programming", "indicator", "speed", "speedometer"],
 };
 
 export const softwareArchitecture: IMXIcon = {
   name: "software-architecture",
   value: require("./svgs/software-architecture.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["software", "architecture", "development", "programming", "horizontal"],
 };
 
 export const softwareInstaller: IMXIcon = {
   name: "software-installer",
   value: require("./svgs/software-installer.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["software", "installer", "development", "programming", "install", "download"],
 };
 
 export const sourceCode: IMXIcon = {
   name: "source-code",
   value: require("./svgs/source-code.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["source", "code", "development", "programming", "html", "web"],
 };
 
 export const systemUpdate: IMXIcon = {
   name: "system-update",
   value: require("./svgs/system-update.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["system", "update", "development", "programming", "software", "arrows", "refresh", "sync"],
 };
 
 export const verifiedAccount: IMXIcon = {
   name: "verified-account",
   value: require("./svgs/verified-account.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["verified", "account", "development", "programming", "certified", "verify", "check", "sign", "badge", "quality"],
 };
 
 export const workflow: IMXIcon = {
   name: "workflow",
   value: require("./svgs/workflow.svg"),
   categories: ["programming"],
-  keywords: ["", ""],
+  keywords: ["workflow", "hierarchy", "development", "programming", "organisation", "process", "diagram", ],
 };
 
 export const programming = [
