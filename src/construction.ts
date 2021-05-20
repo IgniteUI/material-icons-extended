7f33e4c57c1dc256da1222749780a01b46e78298
sdimchevski <sdimchevski@infragistics.com>
2021-05-20 13:44:51 +0300

    Construction icons set added (#42)
    * New icons set - Construction - added
    
    * Added keywords

---
+++ src/construction.ts
@@ -0,0 +1,436 @@
+import { IMXIcon } from "./common/icon";
+
+export const brickWall: IMXIcon = {
+  name: "brick-wall",
+  value: require("./svgs/brick-wall.svg"),
+  categories: ["construction"],
+  keywords: ["brick", "wall", "block", "pattern", "architecture", "stone", "building", "construction", "house"],
+};
+
+export const brush: IMXIcon = {
+  name: "brush",
+  value: require("./svgs/brush.svg"),
+  categories: ["construction"],
+  keywords: ["brush", "decoration", "interior", "paint", "painting", "roller", "wall"],
+};
+
+export const bucket: IMXIcon = {
+  name: "bucket",
+  value: require("./svgs/bucket.svg"),
+  categories: ["construction"],
+  keywords: ["bucket", "decoration", "interior", "paint", "water", "house", "wall", "construction"],
+};
+
+export const building: IMXIcon = {
+  name: "building",
+  value: require("./svgs/building.svg"),
+  categories: ["construction"],
+  keywords: ["building", "home", "house", "apartments", "architecture", "structure"],
+};
+
+export const bulldozer: IMXIcon = {
+  name: "bulldozer",
+  value: require("./svgs/bulldozer.svg"),
+  categories: ["construction"],
+  keywords: ["bulldozer", "digging", "excavator", "machine"],
+};
+
+export const cementMixer: IMXIcon = {
+  name: "cement-mixer",
+  value: require("./svgs/cement-mixer.svg"),
+  categories: ["construction"],
+  keywords: ["cement", "mixer", "concrete", "construction", "machine", "truck"],
+};
+
+export const constructionCone: IMXIcon = {
+  name: "construction-cone",
+  value: require("./svgs/construction-cone.svg"),
+  categories: ["construction"],
+  keywords: ["construction", "cone", "barrier", "caution", "road", "safety"],
+};
+
+export const constructionProject: IMXIcon = {
+  name: "construction-project",
+  value: require("./svgs/construction-project.svg"),
+  categories: ["construction"],
+  keywords: ["construction", "project", "blueprint", "plan", "architecture"],
+};
+
+export const constructionTrolley: IMXIcon = {
+  name: "construction-trolley",
+  value: require("./svgs/construction-trolley.svg"),
+  categories: ["construction"],
+  keywords: ["construction", "trolley", "carrier", "cart", "cement", "holder"],
+};
+
+export const cordlessScrewdriver: IMXIcon = {
+  name: "cordless-screwdriver",
+  value: require("./svgs/cordless-screwdriver.svg"),
+  categories: ["construction"],
+  keywords: ["cordless", "screwdriver", "drill", "driver", "electric", "power"],
+};
+
+export const countryHome: IMXIcon = {
+  name: "country-home",
+  value: require("./svgs/country-home.svg"),
+  categories: ["construction"],
+  keywords: ["country", "home", "house", "real estate"],
+};
+
+export const crane: IMXIcon = {
+  name: "crane",
+  value: require("./svgs/crane.svg"),
+  categories: ["construction"],
+  keywords: ["crane", "construction", "hook", "machinery", "tower"],
+};
+
+export const dumpTruck: IMXIcon = {
+  name: "dump-truck",
+  value: require("./svgs/dump-truck.svg"),
+  categories: ["construction"],
+  keywords: ["dump", "truck", "dumper", "garbage", "trash", "waste", "vehicle", "transport"],
+};
+
+export const estate: IMXIcon = {
+  name: "estate",
+  value: require("./svgs/estate.svg"),
+  categories: ["construction"],
+  keywords: ["estate", "building", "home", "house", "architecture"],
+};
+
+export const forkliftTruck: IMXIcon = {
+  name: "forklift-truck",
+  value: require("./svgs/forklift-truck.svg"),
+  categories: ["construction"],
+  keywords: ["forklift", "truck", "warehouse", "industrial", "transport", "lift", "cargo", "logistics", "loader", "machinery"],
+};
+
+export const foundation: IMXIcon = {
+  name: "foundation",
+  value: require("./svgs/foundation.svg"),
+  categories: ["construction"],
+  keywords: ["foundation", "wall", "architecture", "construction"],
+};
+
+export const fountain: IMXIcon = {
+  name: "fountain",
+  value: require("./svgs/fountain.svg"),
+  categories: ["construction"],
+  keywords: ["fountain", "garden", "splash", "park", "water"],
+};
+
+export const hammer: IMXIcon = {
+  name: "hammer",
+  value: require("./svgs/hammer.svg"),
+  categories: ["construction"],
+  keywords: ["hammer", "construction", "equipment", "hardware", "repair", "tool", "work", "tools"],
+};
+
+export const impactDrill: IMXIcon = {
+  name: "impact-drill",
+  value: require("./svgs/impact-drill.svg"),
+  categories: ["construction"],
+  keywords: ["impact drill", "power", "tools", "driver", "construction", "tool", "work", "repair", "drill"],
+};
+
+export const ladder: IMXIcon = {
+  name: "ladder",
+  value: require("./svgs/ladder.svg"),
+  categories: ["construction"],
+  keywords: ["ladder", "construction", "building", "climb", "height", "tool", "work", "tools"],
+};
+
+export const maintenance: IMXIcon = {
+  name: "maintenance",
+  value: require("./svgs/maintenance.svg"),
+  categories: ["construction"],
+  keywords: ["maintenance", "construction", "tools", "hardware", "equipment", "wrench", "screwdriver", "tool kit", "tool box"],
+};
+
+export const oilPlatform: IMXIcon = {
+  name: "oil-platform",
+  value: require("./svgs/oil-platform.svg"),
+  categories: ["construction"],
+  keywords: ["oil platform", "industry", "drilling", "platform", "oil", "rig", "offshore"],
+};
+
+export const paintRoller: IMXIcon = {
+  name: "paint-roller",
+  value: require("./svgs/paint-roller.svg"),
+  categories: ["construction"],
+  keywords: ["paint roller", "equipment", "house construction", "paint", "roller", "wall paint", "tools", "tool"],
+};
+
+export const pipes: IMXIcon = {
+  name: "pipes",
+  value: require("./svgs/pipes.svg"),
+  categories: ["construction"],
+  keywords: ["pipes", "plumbing", "pipe", "drain", "water"],
+};
+
+export const pipingSystem: IMXIcon = {
+  name: "piping-system",
+  value: require("./svgs/piping-system.svg"),
+  categories: ["construction"],
+  keywords: ["piping system", "piping", "water", "drain", "water", "industrial"],
+};
+
+export const pliers: IMXIcon = {
+  name: "pliers",
+  value: require("./svgs/pliers.svg"),
+  categories: ["construction"],
+  keywords: ["pliers", "equipment", "tools", "tool", "repairs", "hardware"],
+};
+
+export const pneumaticHammerDrill: IMXIcon = {
+  name: "pneumatic-hammer-drill",
+  value: require("./svgs/pneumatic-hammer-drill.svg"),
+  categories: ["construction"],
+  keywords: ["pneumatic hammer drill", "drill", "hammer", "hydraulic", "jack", "kit", "pneumatic", "asphalt"],
+};
+
+export const radiator: IMXIcon = {
+  name: "radiator",
+  value: require("./svgs/radiator.svg"),
+  categories: ["construction"],
+  keywords: ["radiator", "heat", "warm", "interior", "heater", "heating"],
+};
+
+export const restrictedArea: IMXIcon = {
+  name: "restricted-area",
+  value: require("./svgs/restricted-area.svg"),
+  categories: ["construction"],
+  keywords: ["restricted area", "zone", "no entry", "warning sign", "caution zone", "area", "military", "restricted"],
+};
+
+export const roadRoller: IMXIcon = {
+  name: "road-roller",
+  value: require("./svgs/road-roller.svg"),
+  categories: ["construction"],
+  keywords: ["road roller", "construction", "road", "roller", "vehicle", "roadwork"],
+};
+
+export const rulers: IMXIcon = {
+  name: "rulers",
+  value: require("./svgs/rulers.svg"),
+  categories: ["construction"],
+  keywords: ["rulers", "measure", "scale", "measurement", "centimeter", "tools"],
+};
+
+export const safetyVest: IMXIcon = {
+  name: "safety-vest",
+  value: require("./svgs/safety-vest.svg"),
+  categories: ["construction"],
+  keywords: ["safety vest", "construction", "engineer", "jacket", "protect", "safety"],
+};
+
+export const saw: IMXIcon = {
+  name: "saw",
+  value: require("./svgs/saw.svg"),
+  categories: ["construction"],
+  keywords: ["saw", "hand", "tools", "woodworking", "equipment", "tool"],
+};
+
+export const scissorLift: IMXIcon = {
+  name: "scissor-lift",
+  value: require("./svgs/scissor-lift.svg"),
+  categories: ["construction"],
+  keywords: ["scissor lift", "lift", "scaffolding", "scissor", "hydraulic", "construction", "high", "factory", "industry"],
+};
+
+export const screwdriver: IMXIcon = {
+  name: "screwdriver",
+  value: require("./svgs/screwdriver.svg"),
+  categories: ["construction"],
+  keywords: ["screwdriver", "tools", "tool", "hardware", "screw", "fixer", "repair"],
+};
+
+export const shovel: IMXIcon = {
+  name: "shovel",
+  value: require("./svgs/shovel.svg"),
+  categories: ["construction"],
+  keywords: ["shovel", "digging", "tool", "spade", "equipment"],
+};
+
+export const skyscrapers: IMXIcon = {
+  name: "skyscrapers",
+  value: require("./svgs/skyscrapers.svg"),
+  categories: ["construction"],
+  keywords: ["skyscrapers", "buildings", "city", "cityscape", "downtown", "skyline", "architecture"],
+};
+
+export const solarPanel: IMXIcon = {
+  name: "solar-panel",
+  value: require("./svgs/solar-panel.svg"),
+  categories: ["construction"],
+  keywords: ["solar panel", "alternative", "energy", "electricity", "panel", "solar"],
+};
+
+export const solderingIron: IMXIcon = {
+  name: "soldering-iron",
+  value: require("./svgs/soldering-iron.svg"),
+  categories: ["construction"],
+  keywords: ["soldering iron", "soldering", "iron", "tool", "copper", "manufactoring", "electronic"],
+};
+
+export const stairs: IMXIcon = {
+  name: "stairs",
+  value: require("./svgs/stairs.svg"),
+  categories: ["construction"],
+  keywords: ["stairs", "building", "climb", "height"],
+};
+
+export const stopHand: IMXIcon = {
+  name: "stop-hand",
+  value: require("./svgs/stop-hand.svg"),
+  categories: ["construction"],
+  keywords: ["stop hand", "stop", "sign", "halt", "adblock"],
+};
+
+export const tableSawBlade: IMXIcon = {
+  name: "table-saw-blade",
+  value: require("./svgs/table-saw-blade.svg"),
+  categories: ["construction"],
+  keywords: ["table saw", "blade", "cut", "saw", "table"],
+};
+
+export const tapWater: IMXIcon = {
+  name: "tap-water",
+  value: require("./svgs/tap-water.svg"),
+  categories: ["construction"],
+  keywords: ["tap water", "tap", "water", "faucet", "drip", "leak"],
+};
+
+export const tapeline: IMXIcon = {
+  name: "tapeline",
+  value: require("./svgs/tapeline.svg"),
+  categories: ["construction"],
+  keywords: ["tapeline", "centimeter", "inches tape", "measuring tape", "meter stick", "inches", "measure", "metre", "ruler"],
+};
+
+export const towTruck: IMXIcon = {
+  name: "tow-truck",
+  value: require("./svgs/tow-truck.svg"),
+  categories: ["construction"],
+  keywords: ["tow truck", "repair", "service", "tow", "truck", "towing", "vehicle", "transport"],
+};
+
+export const towelRail: IMXIcon = {
+  name: "towel-rail",
+  value: require("./svgs/towel-rail.svg"),
+  categories: ["construction"],
+  keywords: ["towel rail", "warmer", "heated", "towel", "rail", "bathroom", "interior", "rack"],
+};
+
+export const tractor: IMXIcon = {
+  name: "tractor",
+  value: require("./svgs/tractor.svg"),
+  categories: ["construction"],
+  keywords: ["tractor", "vehicle", "machinery"],
+};
+
+export const trowel: IMXIcon = {
+  name: "trowel",
+  value: require("./svgs/trowel.svg"),
+  categories: ["construction"],
+  keywords: ["trowel", "cement", "equipment", "tool", "renovation", "maintenance"],
+};
+
+export const waterBoiler: IMXIcon = {
+  name: "water-boiler",
+  value: require("./svgs/water-boiler.svg"),
+  categories: ["construction"],
+  keywords: ["water boiler", "boiler", "heater", "water", "heating", "appliance"],
+};
+
+export const waterHeater: IMXIcon = {
+  name: "water-heater",
+  value: require("./svgs/water-heater.svg"),
+  categories: ["construction"],
+  keywords: ["water heater", "heater", "water", "tank", "appliance", "electrical", "heating"],
+};
+
+export const waterTap: IMXIcon = {
+  name: "water-tap",
+  value: require("./svgs/water-tap.svg"),
+  categories: ["construction"],
+  keywords: ["water tap", "tap", "faucet", "water", "plumbing", "valve"],
+};
+
+export const worker: IMXIcon = {
+  name: "worker",
+  value: require("./svgs/worker.svg"),
+  categories: ["construction"],
+  keywords: ["worker", "builder", "engineer", "man", "labor"],
+};
+
+export const workers: IMXIcon = {
+  name: "workers",
+  value: require("./svgs/workers.svg"),
+  categories: ["construction"],
+  keywords: ["workers", "crew", "builders", "engineers", "men", "constructors"],
+};
+
+export const wrench: IMXIcon = {
+  name: "wrench",
+  value: require("./svgs/wrench.svg"),
+  categories: ["construction"],
+  keywords: ["wrench", "construction", "equipment", "hardware", "metal", "tool"],
+};
+
+export const construction = [
+  brickWall,
+  brush,
+  bucket,
+  building,
+  bulldozer,
+  cementMixer,
+  constructionCone,
+  constructionProject,
+  constructionTrolley,
+  cordlessScrewdriver,
+  countryHome,
+  crane,
+  dumpTruck,
+  estate,
+  forkliftTruck,
+  foundation,
+  fountain,
+  hammer,
+  impactDrill,
+  ladder,
+  maintenance,
+  oilPlatform,
+  paintRoller,
+  pipes,
+  pipingSystem,
+  pliers,
+  pneumaticHammerDrill,
+  radiator,
+  restrictedArea,
+  roadRoller,
+  rulers,
+  safetyVest,
+  saw,
+  scissorLift,
+  screwdriver,
+  shovel,
+  skyscrapers,
+  solarPanel,
+  solderingIron,
+  stairs,
+  stopHand,
+  tableSawBlade,
+  tapWater,
+  tapeline,
+  towTruck,
+  towelRail,
+  tractor,
+  trowel,
+  waterBoiler,
+  waterHeater,
+  waterTap,
+  worker,
+  workers,
+  wrench,
+];
